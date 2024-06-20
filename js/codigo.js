cdf33c0c54d5feb00fe9fdd005f37fbf3c4d631b
Laura Del Rei <160788465+Lhauu@users.noreply.github.com>
2024-06-20 20:50:41 -0300

    ok

--- js/codigo.js
+++ js/codigo.js
@@ -5,7 +5,32 @@ let times = [
     { nome: "Alemanha", ranking: 1647.52, forca: 0, grupo: "A" },
     { nome: "Escocia", ranking: 1461.55, forca: 0, grupo: "A" },
     { nome: "Hungria", ranking: 1537.65, forca: 0, grupo: "A" },
-    { nome: "suiça", ranking: 1593.49,forca: 0, grupo: "A" },
+    { nome: "Suica", ranking: 1593.49,forca: 0, grupo: "A" },
+
+    { nome: "Espanha", ranking: 1715.22, forca: 0, grupo "B"},
+    { nome: "Croscia", ranking: 1704.23, forca: 0, grupo: "B"},
+    { nome: "Italia", ranking: 1713.66, forca: 0, grupo: "B"},
+    { nome: "Albania", ranking: 1351.53, forca: 0, grupo: "B"},
+
+    { nome: "Eslovenia", ranking: 1427.84, forca: 0, grupo: "C"},
+    { nome: "Dinamarca", ranking: 1602.72, forca: 0, grupo: "C"},
+    { nome: "Servia", ranking: 1496.53, forca: 0, grupo: "C"},
+    { nome: "Inglaterra", ranking: 1794.01, forca: 0, grupo: "C"},
+
+    { nome: "Paises Baixos", ranking: 1731.23, forca: 0, grupo: "D"},
+    { nome: "Franca", ranking: 1840.59, forca: 0, grupo: "D"},
+    { nome: "Polonia", ranking: 1534.66, forca: 0, grupo: "D"},
+    { nome: "Austria", ranking: 1546.12, forca: 0, grupo: "D"},
+
+    { nome: "Romenia", ranking: 1433.71, forca: 0, grupo: "E"},
+    { nome: "Belgica", ranking: 1795.23, forca: 0, grupo: "E"},
+    { nome: "Eslovaquia", ranking: 1427.15, forca: 0, grupo: "E"},
+    { nome: "Ucrania", ranking: 1561.95, forca: 0, grupo: "E"},
+    
+    { nome: "Portugal", ranking: 1742.56, forca: 0, grupo: "F"},
+    { nome: "Chequia", ranking: 1482.12, forca: 0, grupo: "F"},
+    { nome: "Georgia", ranking: 1287.43, forca: 0, grupo: "F"},
+    { nome: "Turquia", ranking: 1457.92, forca: 0, grupo: "F"},
 ];
 let gola=0;
 let golb=0;
